9f1f92b477a8429a8d0167a5970cf023ff38af6d
joujonikiasaroy <joujonikiasaroy@gmail.com>
2023-09-18 21:08:24 +0600

    Added hungry monster project

---
+++ app.js
@@ -0,0 +1,10 @@
+var image = document.getElementById("image");
+var image1 = document.getElementById("image1");
+var image2 = document.getElementById("image2");
+var image3 = document.getElementById("image3");
+function imageOfMeal(){
+    image.src = "./images/logo.svg";
+    image1.src = "./images/logo.svg";
+    image2.src = "./images/logo.svg";
+    image3.src = "./images/logo.svg";
+}
